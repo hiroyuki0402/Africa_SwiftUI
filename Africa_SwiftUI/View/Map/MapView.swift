7ff1543fd14a0f6c26706cd15d8102cd0129069e
hiroyuki0402 <hiro1990.cloud@gmail.comcd>
2023-10-28 21:29:58 +0900

    5日目 - 完成

--- Africa_SwiftUI/View/Map/MapView.swift
+++ Africa_SwiftUI/View/Map/MapView.swift
@@ -6,14 +6,32 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct MapView: View {
     // MARK: - プロパティー
 
+    @State private var region: MKCoordinateRegion = {
+        let mapCoordinate = CLLocationCoordinate2D(latitude: 6.600286, longitude: 16.4377599)
+        let MapZoomLevel = MKCoordinateSpan(latitudeDelta: 70, longitudeDelta: 70)
+        let mapRegion = MKCoordinateRegion(
+                center: mapCoordinate,
+                span: MapZoomLevel)
+        return mapRegion
+    }()
+
+    @StateObject var mapViewModel = MapViewModel()
+
     // MARK: - ボディー
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Map {
+            ForEach(mapViewModel.mapDatas) { item in
+                Annotation(item.name, coordinate: item.location) {
+                    MapAnnotationView(mapData: item)
+                }
+            }
+        }
     }
 }
 
